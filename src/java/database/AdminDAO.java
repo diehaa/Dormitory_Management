5d164456cff1cde2c7ce40c396d59114141172c5
diehaa <110818909+diehaa@users.noreply.github.com>
2023-11-29 21:34:03 +0700

    Update AdminDAO.java

--- src/java/database/AdminDAO.java
+++ src/java/database/AdminDAO.java
@@ -1,1 +1,208 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Admin;
+
+/**
+ *
+ * @author phangiabao
+ */
+public class AdminDAO {
+
+    public ArrayList<Admin> getListTaiKhoanAdmin() {
+        ArrayList<Admin> list = new ArrayList<>();
+        String query = "select * from admin";
+        try {
+
+            Connection conn = new JDBCUtil().getConnection();
+            PreparedStatement ps = conn.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+
+                list.add(new Admin(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8)));
+            }
+
+        } catch (Exception e) {
+        }
+        return list;
+    }
+
+    public Admin getListTaiKhoanAdminById(Admin t) {
+        Admin ketQua = null;
+        try {
+            final Connection con = JDBCUtil.getConnection();
+            final String sql = "SELECT * FROM admin WHERE adminId=?";
+            final PreparedStatement st = con.prepareStatement(sql);
+            st.setInt(1, t.getAdminId());
+            System.out.println(sql);
+            final ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                ketQua = new Admin(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8));
+            }
+
+        } catch (Exception e) {
+        }
+        return ketQua;
+    }
+
+    public Admin getListTaiKhoanAdminByIdString(String t) {
+        Admin ketQua = null;
+        try {
+            final Connection con = JDBCUtil.getConnection();
+            final String sql = "SELECT * FROM admin WHERE adminId=?";
+            final PreparedStatement st = con.prepareStatement(sql);
+            st.setString(1, t);
+            System.out.println(sql);
+            final ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                ketQua = new Admin(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8));
+            }
+
+        } catch (Exception e) {
+        }
+        return ketQua;
+    }
+
+    public int delete(String t) {
+        int ketQua = 0;
+        try {
+            final Connection con = JDBCUtil.getConnection();
+            final String sql = "DELETE from admin  WHERE adminId=?";
+            final PreparedStatement st = con.prepareStatement(sql);
+            st.setString(1, t);
+            System.out.println(sql);
+            ketQua = st.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ketQua;
+    }
+
+    public int insert(Admin t) {
+        int ketQua = 0;
+        try {
+            final Connection con = JDBCUtil.getConnection();
+            final String sql = "INSERT INTO admin (adminId, username, password, name, email, role,phone,avatar)  VALUES (?,?,?,?,?,?,?,?)";
+            final PreparedStatement st = con.prepareStatement(sql);
+            st.setInt(1, t.getAdminId());
+            st.setString(2, t.getUsername());
+            st.setString(3, t.getPassword());
+            st.setString(4, t.getName());
+            st.setString(5, t.getEmail());
+            st.setString(6, t.getRole());
+            st.setString(7, t.getPhone());
+            st.setString(8, t.getAvatar());
+            ketQua = st.executeUpdate();
+            System.out.println("B\u1ea1n \u0111\u00e3 th\u1ef1c thi: " + sql);
+            System.out.println("C\u00f3 " + ketQua + " d\u00f2ng b\u1ecb thay \u0111\u1ed5i!");
+            JDBCUtil.closeConnection(con);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ketQua;
+    }
+
+    public int update(Admin t) {
+        int ketQua = 0;
+        try {
+            final Connection con = JDBCUtil.getConnection();
+            final String sql = "UPDATE admin  SET  name=?, email=?,role=?,phone=? WHERE adminId=?";
+            final PreparedStatement st = con.prepareStatement(sql);
+            st.setString(1, t.getName());
+            st.setString(2, t.getEmail());
+            st.setString(3, t.getRole());
+            st.setString(4, t.getPhone());
+            st.setInt(5, t.getAdminId());
+            System.out.println(sql);
+            ketQua = st.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ketQua;
+    }
+
+    public Admin selectByUserAndPassword(Admin t) {
+        Admin ketQua = null;
+        try {
+            final Connection con = JDBCUtil.getConnection();
+            final String sql = "SELECT * FROM admin WHERE username=? and password=?";
+            final PreparedStatement st = con.prepareStatement(sql);
+            st.setString(1, t.getUsername());
+            st.setString(2, t.getPassword());
+            System.out.println(sql);
+            final ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                ketQua = new Admin(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8));
+            }
+
+        } catch (Exception e) {
+        }
+        return ketQua;
+    }
+
+    public Admin selectByEmail(Admin t) {
+        Admin ketQua = null;
+        try {
+            final Connection con = JDBCUtil.getConnection();
+            final String sql = "SELECT * FROM admin WHERE email=?";
+            final PreparedStatement st = con.prepareStatement(sql);
+            st.setString(1, t.getEmail());
+            System.out.println(sql);
+            final ResultSet rs = st.executeQuery();
+            if (rs.next()) {
+                ketQua = new Admin(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7), rs.getString(8));
+            }
+
+        } catch (Exception e) {
+        }
+        return ketQua;
+    }
+
+    public boolean kiemTraTenDangNhap(final String username) {
+        boolean ketQua = false;
+        try {
+            final Connection con = JDBCUtil.getConnection();
+            final String sql = "SELECT * FROM admin WHERE username=?";
+            final PreparedStatement st = con.prepareStatement(sql);
+            st.setString(1, username);
+            System.out.println(sql);
+            final ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                ketQua = true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ketQua;
+    }
+
+    public int changePassword(Admin t) {
+        int ketQua = 0;
+        try {
+            final Connection con = JDBCUtil.getConnection();
+            final String sql = "UPDATE admin  SET  password=? WHERE adminId=?";
+            final PreparedStatement st = con.prepareStatement(sql);
+            st.setString(1, t.getPassword());
+            st.setInt(2, t.getAdminId());
+            System.out.println(sql);
+            ketQua = st.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ketQua;
+    }
+
+}
 
