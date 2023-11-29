b1595bcf95014347b6a199677635ddcbfc489cf7
diehaa <110818909+diehaa@users.noreply.github.com>
2023-11-29 21:32:50 +0700

    Add files via upload

---
+++ src/java/controller/Constants.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+/**
+ *
+ * @author phangiabao
+ */
+public class Constants {
+    public static String GOOGLE_CLIENT_ID = "1070962791948-v31k9v3iqe31r9b9ad5m3chg7hv28i3i.apps.googleusercontent.com";
+
+	public static String GOOGLE_CLIENT_SECRET = "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";
+
+	public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/AssignmentPRJ301/login-google";
+
+	public static String GOOGLE_LINK_GET_TOKEN = "https://accounts.google.com/o/oauth2/token";
+
+	public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+
+	public static String GOOGLE_GRANT_TYPE = "authorization_code";
+}
